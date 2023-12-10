0949a7880428c2f23b841648800709485aee7ad4
cryptonerdcn <cryptonerdcn@gmail.com>
2023-12-10 13:35:42 +0900

    Update: UI and Logo.

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,40 @@
 import './App.css'
 // import WebApp from '@twa-dev/sdk'
 
+import ARKLogo from './assets/Subtract.svg'
+import ARKText from './assets/arktext.svg'
+
+
 function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-blue-100 p-4">
-        <header className="flex justify-between items-center py-4">
-          <i className="fas fa-bars text-xl"></i>
-          <img src="https://placehold.co/50x50" alt="Ark logo" className="rounded-full" />
+      <div className="min-h-screen bg-blue-100 p-8">
+        <header className="flex items-center py-4">
+          <img src={ARKLogo} alt="Ark logo" className="" />
+          <img src={ARKText} alt="Ark logo" className="" />
         </header>
-        <div className="bg-gradient-to-r from-blue-300 via-pink-300 to-yellow-300 p-6 rounded-3xl shadow-lg mb-6">
+        <div className="bg-gradient-to-r from-blue-300 via-pink-300 to-yellow-300 p-6 rounded-3xl shadow-lg mb-6 h-[30vh]">
           <div className="flex justify-between items-center">
             <div className="text-white">
               <div className="bg-white bg-opacity-20 rounded p-1 w-12 mb-2"></div>
               <h1 className="text-2xl font-bold">Ark Protocol</h1>
             </div>
-            <div className="space-y-2">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
           </div>
         </div>
-        <div className="flex justify-between mb-6">
+        <div className="flex justify-between p-8">
           <button className="text-center focus:outline-none">
             <i className="fas fa-dollar-sign text-blue-500 text-3xl mb-2"></i>
             <p className="text-sm">Lending</p>
           </button>
-          <div className="text-center">
+          <button className="text-center focus:outline-none">
             <i className="fas fa-hand-holding-usd text-blue-500 text-3xl mb-2"></i>
             <p className="text-sm">Borrowing</p>
-          </div>
-          <div className="text-center">
+          </button>
+          <button className="text-center focus:outline-none">
             <i className="fas fa-tags text-blue-500 text-3xl mb-2"></i>
             <p className="text-sm">Bargain Link</p>
-          </div>
+          </button>
         </div>
         <div className="bg-white p-6 rounded-3xl shadow-lg mb-6">
           <div className="flex justify-between items-center mb-4">

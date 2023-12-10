4577f2ca5065de9269e360ff50ddd0f0b8f7c21f
cryptonerdcn <cryptonerdcn@gmail.com>
2023-12-10 13:12:53 +0900

    Fix.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-
-import WebApp from '@twa-dev/sdk'
+// import WebApp from '@twa-dev/sdk'
 
 function App() {
 
